8f348ce4d193df08b3ca4596eb702b1570b43043
Lorenzo <lorenzofumi@gmail.com>
2024-06-20 18:42:01 +0200

    Changed port to 5000

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -15,5 +15,16 @@ export default defineConfig({
   },
   server: {
     host: '0.0.0.0',
-  }
+    port: 5000
+  },
+  preview: {
+    port: 5000
+  },
+  devServer: {
+    open: process.platform === 'darwin',
+    host: '0.0.0.0',
+    port: 5000, // CHANGE YOUR PORT HERE!
+    https: false,
+    hotOnly: false,
+  },
 })
